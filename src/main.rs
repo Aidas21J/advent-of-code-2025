3a0be519b3bce677bef0cdab17138fa33c65627e
AidasJ21 <aidas.jurelevicius@gmail.com>
2025-12-01 13:51:57 +0200

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ fn main() {
     let input = fs::read_to_string("input/day1.txt")
         .expect("failed to read input");
 
-    let answer1 = day1::part1(input).expect("part 1 failed");
+    let answer1 = day1::part1(&input).expect("part 1 failed");
+    let answer2 = day1::part2(&input).expect("part 2 failed");
 
     println!("Part 1: {answer1}");
+    println!("Part 2: {answer2}");
 }
 
--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -6,15 +6,15 @@ const DIAL_START_POSITION: u64 = 50;
 #[derive(Debug, PartialEq, Eq)]
 struct Dial {
     position: u64,
+    point_at_zero_count: u64,
 }
 
 impl Dial {
     fn new() -> Dial {
-        Dial { position: DIAL_START_POSITION }
-    }
-
-    fn at_zero(&self) -> bool {
-        self.position == 0
+        Dial { 
+            position: DIAL_START_POSITION,
+            point_at_zero_count: 0,
+        }
     }
 }
 
@@ -45,7 +45,7 @@ impl FromStr for Rotation {
             None        => return Err("no rotation direction provided".to_owned()),
         };
 
-        let amount = match s_iterator.as_str().parse::<u64>() {
+        let amount = match s_iterator.as_str().parse() {
             Ok(a) => a,
             Err(err) => return Err(format!("failed to parse rotation amount: {err}")),
         };
@@ -59,18 +59,35 @@ impl FromStr for Rotation {
 
 impl Dial {
     fn turn(&mut self, rotation: &Rotation) {
-        let turn_right_amount = match rotation.direction {
-            RotationDirection::LEFT     => FULL_ROTATION - (rotation.amount % FULL_ROTATION),
-            RotationDirection::RIGHT    => rotation.amount,
-        };
+        if rotation.direction == RotationDirection::LEFT {
+            self.position = (FULL_ROTATION - self.position) % FULL_ROTATION;
+        }
 
-        self.position = (self.position + turn_right_amount) % FULL_ROTATION;
+        self.position = (self.position + rotation.amount) % FULL_ROTATION;
+
+        if rotation.direction == RotationDirection::LEFT {
+            self.position = (FULL_ROTATION - self.position) % FULL_ROTATION;
+        }
+        self.point_at_zero_count += (self.position == 0) as u64;
+    }
+
+    fn turn_with_intermediate_zero(&mut self, rotation: &Rotation) {
+        if rotation.direction == RotationDirection::LEFT {
+            self.position = (FULL_ROTATION - self.position) % FULL_ROTATION;
+        }
+
+        let sum = self.position + rotation.amount;
+        self.position = sum % FULL_ROTATION;
+        self.point_at_zero_count += sum / FULL_ROTATION;
+
+        if rotation.direction == RotationDirection::LEFT {
+            self.position = (FULL_ROTATION - self.position) % FULL_ROTATION;
+        }
     }
 }
 
-pub fn part1(input: String) -> Result<u64, String> {
+pub fn part1(input: &String) -> Result<u64, String> {
     let mut dial = Dial::new();
-    let mut number_if_zeroes: u64 = 0;
 
     for line in input.lines() {
         let rotation = match line.trim().parse::<Rotation>() {
@@ -79,12 +96,24 @@ pub fn part1(input: String) -> Result<u64, String> {
         };
 
         dial.turn(&rotation);
-        if dial.at_zero() {
-            number_if_zeroes += 1;
-        }
     }
 
-    return Ok(number_if_zeroes);
+    return Ok(dial.point_at_zero_count);
+}
+
+pub fn part2(input: &String) -> Result<u64, String> {
+    let mut dial = Dial::new();
+
+    for line in input.lines() {
+        let rotation = match line.trim().parse::<Rotation>() {
+            Ok(r) => r,
+            Err(err) => return Err(err),
+        };
+
+        dial.turn_with_intermediate_zero(&rotation);
+    }
+
+    return Ok(dial.point_at_zero_count);
 }
 
 #[cfg(test)]
@@ -104,6 +133,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(EXAMPLE_INPUT.to_owned()), Ok(3));
+        assert_eq!(part1(&EXAMPLE_INPUT.to_owned()), Ok(3));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&EXAMPLE_INPUT.to_owned()), Ok(6));
     }
 }
